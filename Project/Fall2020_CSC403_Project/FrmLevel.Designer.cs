3065b30e30228aa9625e863dd5edef2926bad6a1
Beverly <bcoronelp@gmail.com>
2020-11-18 13:09:49 -0600

    implemented main menu feature

--- Project/Fall2020_CSC403_Project/FrmLevel.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmLevel.Designer.cs
@@ -323,6 +323,7 @@ namespace Fall2020_CSC403_Project {
       this.Load += new System.EventHandler(this.FrmLevel_Load);
       this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmLevel_KeyDown);
       this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FrmLevel_KeyUp);
+            this.FormClosing += FrmLevel_FormClosing;
       ((System.ComponentModel.ISupportInitialize)(this.picEnemyCheeto)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.picEnemyPoisonPacket)).EndInit();
       ((System.ComponentModel.ISupportInitialize)(this.picWall3)).EndInit();
--- Project/Fall2020_CSC403_Project/FrmLevel.cs
+++ Project/Fall2020_CSC403_Project/FrmLevel.cs
@@ -21,7 +21,12 @@ namespace Fall2020_CSC403_Project {
       InitializeComponent();
     }
 
-    private void FrmLevel_Load(object sender, EventArgs e) {
+  private void FrmLevel_FormClosing(object sender, FormClosingEventArgs e)
+  {
+            Application.Exit();
+  }
+
+        private void FrmLevel_Load(object sender, EventArgs e) {
       const int PADDING = 7;
       const int NUM_WALLS = 13;
 
